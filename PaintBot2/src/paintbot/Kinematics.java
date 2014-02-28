6c1e507d222f91c1809a2a0780d6b5e06d6bfadb
brianh014 <brianh014@gmail.com>
2014-02-27 18:28:02 -0600

    Added PaintBot2 project folder for project 2 and updated the interface.
    Added:
    -Function in kinematics to move a4
    -Added control buttons to move a4
    -Added a mouse follow toggle switch
    -When mouse follow is toggled, dragging mouse and clicking will paint

---
+++ PaintBot2/src/paintbot/Kinematics.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paintbot;
+
+import java.lang.Math;
+
+/**
+ *
+ * @author Brian
+ */
+public class Kinematics {
+    
+    public final int base_x = 228;
+    public final int base_y = 500;
+    
+    public double a1_x;
+    public double a1_y;
+    public double a2_x;
+    public double a2_y;
+    public double a3_x;
+    public double a3_y;
+    public double a4_x;
+    public double a4_y;
+    
+    public double a2_th;
+    public double a3_th;
+    public double d1;
+    
+    /**
+     * Set all axis coordinates to default positions
+     */
+    public Kinematics(){
+        a1_x = base_x;
+        a1_y = base_y;
+        a2_x = a1_x;
+        a2_y = a1_y - 150;
+        a3_x = a2_x + 100;
+        a3_y = a2_y;
+        a4_x = a3_x;
+        a4_y = a3_y + 75;
+        a2_th = -90;
+        a3_th = -90;
+        d1 = 0;
+    }
+    
+    /**
+     * 
+     * @param positive if axis 1 should slide in the positive direction
+     */
+    public void axis1_slide(boolean positive){
+        
+        int d1_change;
+        
+        if(positive == true && d1 < 300) {
+            d1 = d1 + 1;
+            d1_change = 1;
+        } else if (positive == false && d1 > 0) {
+            d1 = d1 - 1;
+            d1_change = -1;
+        }
+        else
+        {
+            d1_change = 0;
+        }
+        
+        a1_x = a1_x + d1_change;
+        a2_x = a2_x + d1_change;
+        a3_x = a3_x + d1_change;
+        a4_x = a4_x + d1_change;  
+    }
+    
+    /**
+     * 
+     * @param positive if axis 2 should rotate in the positive direction
+     */
+    public void axis2_rotate(boolean positive){
+        
+        if(positive == true)
+            a2_th = a2_th + 1;  
+        else
+            a2_th = a2_th - 1;
+
+        //Y has inversed values due to coordinate system of the frame
+        a3_y = -100 * Math.cos(a2_th * (Math.PI/180)) - 150 + base_y;
+        a3_x = -100 * Math.sin(a2_th * (Math.PI/180)) + d1 + base_x;
+        
+        a4_y = -75 * (Math.cos(a2_th * (Math.PI/180)) * Math.cos(a3_th * (Math.PI/180)) 
+                - Math.sin(a2_th * (Math.PI/180)) * Math.sin(a3_th * (Math.PI/180)))
+                - 100 * Math.cos(a2_th * (Math.PI/180)) - 150 + base_y;
+        
+        a4_x = 75 * (-Math.sin(a2_th * (Math.PI/180)) * Math.cos(a3_th * (Math.PI/180)) 
+                - Math.cos(a2_th * (Math.PI/180)) * Math.sin(a3_th * (Math.PI/180)))
+                - 100 * Math.sin(a2_th * (Math.PI/180)) + d1 + base_x;
+    }
+    
+    /**
+     * 
+     * @param positive if axis 3 should rotate in the positive direction
+     */
+    public void axis3_rotate(boolean positive){
+        
+        if(positive == true)
+            a3_th = a3_th + 1;  
+        else
+            a3_th = a3_th - 1;        
+        
+        a4_y = -75 * (Math.cos(a2_th * (Math.PI/180)) * Math.cos(a3_th * (Math.PI/180)) 
+                - Math.sin(a2_th * (Math.PI/180)) * Math.sin(a3_th * (Math.PI/180)))
+                - 100 * Math.cos(a2_th * (Math.PI/180)) - 150 + base_y;
+        
+        a4_x = 75 * (-Math.sin(a2_th * (Math.PI/180)) * Math.cos(a3_th * (Math.PI/180)) 
+                - Math.cos(a2_th * (Math.PI/180)) * Math.sin(a3_th * (Math.PI/180)))
+                - 100 * Math.sin(a2_th * (Math.PI/180)) + d1 + base_x;
+    }
+    
+    /**
+     * Function to move paint head to given coordinates
+     * @param x 
+     * @param y
+     */
+    public void head_move(double x, double y){
+        
+        //This section is for testing only, remove when actual functionality is added
+        a4_x = x;
+        a4_y = y;
+        //
+    }
+    
+}
